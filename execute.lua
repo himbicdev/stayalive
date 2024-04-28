398e79b368d1a6ac95aa51cd1c057edaf519399d
himbicdev <168370657+himbicdev@users.noreply.github.com>
2024-04-28 15:15:23 -0400

    Create execute.lua

---
+++ execute.lua
@@ -0,0 +1,11 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "Script Loading"; -- the title 
+Text = "himbic edit"; -- what the text says 
+Duration = 5; -- how long the notification should in secounds
+})
+
+
+wait(3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/himbicdev/stayalive/main/paid.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/himbicdev/stayalive/main/infyield.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/himbicdev/stayalive/main/spychat.lua"))()
