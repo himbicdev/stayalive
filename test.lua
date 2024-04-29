1e199e5adf01135c04d8685d3b2892f6899fdb64
himbicdev <168370657+himbicdev@users.noreply.github.com>
2024-04-29 17:23:28 -0400

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,5 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "Script Loading"; -- the title 
+Text = "himbic edit"; -- what the text says 
+Duration = 5; -- how long the notification should in secounds
+})
